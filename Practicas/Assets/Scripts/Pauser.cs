4cf48056eaa8f62ed04baec076d6981c8468d766
Christian Justo <cjusto@uabc.edu.mx>
2018-08-24 19:43:06 -0700

    Menu de Pausa Agregado
    SE agregaron tres botones continuar, reset y mute

--- Practicas/Assets/Scripts/Pauser.cs
+++ Practicas/Assets/Scripts/Pauser.cs
@@ -1,19 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Pauser : MonoBehaviour {
 	private bool paused = false;
+	public GameObject pausePanel;
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyUp(KeyCode.P))
 		{
 			paused = !paused;
+			pausePanel.SetActive(paused);
 		}
 
 		if(paused)
 			Time.timeScale = 0;
 		else
 			Time.timeScale = 1;
+	}
+
+	public void ContinueGame(){
+
+		Debug.Log("Continuar");
+		paused = false;
+		pausePanel.SetActive(paused);
+	}
+	public void ResetGame(){
+
+		Debug.Log("ResetGame");
+		SceneManager.LoadScene(0);
+
+	}
+	public void unMusicGame(){
+
+		Debug.Log("unMusicGame");
 	}
 }
