187ad58e65d14d0a29211d2993ce622cef88e038
Christian Justo <cjusto@uabc.edu.mx>
2018-09-14 00:54:14 -0700

    Practica 3
    interacción con objetos

---
+++ Practicas/Assets/Scripts/Interactable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour 
+{
+    public Color color;
+    public GameObject infoPanel;
+    private bool isPlayerInside = false;
+    public float rotationSpeed = 3f;
+
+    private void Start()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+
+        if(Input.GetKeyDown(KeyCode.I) && isPlayerInside)
+        {
+            gameObject.GetComponent<Renderer>().material.color = color;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            infoPanel.SetActive(true);
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            infoPanel.SetActive(false);
+            isPlayerInside = false;
+        }
+    }
+}
---
+++ Practicas/Assets/Scripts/Interactable2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interactable2 : MonoBehaviour 
+{
+    public Color color;
+    public GameObject infoPanel;
+    private bool isPlayerInside = false;
+    public float rotationSpeed = 3f;
+
+    private void Start()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+
+        if(Input.GetKeyDown(KeyCode.E) && isPlayerInside)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            infoPanel.SetActive(true);
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            infoPanel.SetActive(false);
+            isPlayerInside = false;
+        }
+    }
+}
